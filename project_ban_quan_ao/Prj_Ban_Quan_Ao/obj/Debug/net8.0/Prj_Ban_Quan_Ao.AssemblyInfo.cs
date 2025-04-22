04a09962931981944e9846f11783c096ed075365
hoangsobi <hoangsobi662003@gmail.com>
2025-04-22 21:21:15 +0700

    update logo

--- project_ban_quan_ao/Prj_Ban_Quan_Ao/obj/Debug/net8.0/Prj_Ban_Quan_Ao.AssemblyInfo.cs
+++ project_ban_quan_ao/Prj_Ban_Quan_Ao/obj/Debug/net8.0/Prj_Ban_Quan_Ao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prj_Ban_Quan_Ao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f562ca59d379e0876ed33c1f78e465dcc64fcb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356cdceb5a0eee59be80a8aa02fe644e2a44d9fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prj_Ban_Quan_Ao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prj_Ban_Quan_Ao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
